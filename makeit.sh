25f07a4c08f18c1f3423f89e2dc71894169caf55
Jason R. Coombs <jaraco@jaraco.com>
2017-01-05 03:47:48 -0500

    More simply generate the virtualenv and replicate the effect of cwd in 'nspace-two' by setting PYTHONPATH.

--- makeit.sh
+++ makeit.sh
@@ -1,26 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
-find ~/.cache/pip/wheels/ -name '*nspace*' | xargs rm -f
-mkdir -p ~/nspace-dist
-rm -f  ~/nspace-dist/*
-rm -rf /tmp/testvenv
+rm -R env | echo -n
 
-if [ ! -f ~/.pip/pip.conf ] || ! grep -q nspace-dist ~/.pip/pip.conf ; then
-  echo 'For this to work, create ~/.pip/pip.conf with the line:'
-  echo '[global]'
-  echo 'find-links = /home/vagrant/nspace-dist'
-  exit 1
-fi
+python3 -m virtualenv env
 
-(cd nspace-two; python setup.py sdist -d ~/nspace-dist)
-(cd nspace-three; python setup.py sdist -d ~/nspace-dist)
+env/bin/pip install ./nspace-three ./nspace-two
 
-(
- virtualenv -p $(which python3) /tmp/testvenv 
- source /tmp/testvenv/bin/activate
- pip install ~/nspace-dist/nspace-two-1.0.0.tar.gz
- set -x
- cd nspace-two
- python -c 'from nspace.three.morestuff import *'  # Always works
- python -c 'from nspace.two.stuff import *'        # Breaks on 3.5
-)
+PYTHONPATH=nspace-two env/bin/python -c 'from nspace.two.stuff import *'
