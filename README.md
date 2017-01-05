ce8d472416c2051f35d2c702d82ca86d2e83c371
Iwan Vosloo <iwan@reahl.org>
2017-01-05 09:32:34 +0200

    Reproduced issue without tox

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Reproducing a bug
 
 This is, I think, a corner case triggering a bug in setuptools related to namespace packages.
 
-Run ./makeit.sh to see it break on py35 (you need tox)
+Run ./makeit.sh to see it break on py35
 
 The bug can be bypassed in two ways:
- - by getting tox to chdir out of nspace-two directory before it runs the imports
+ - by chdir-ing out of nspace-two directory before you run the imports
  - by commenting out the "import pkg_resources" in nspace-two/nspace/two/stuff.py
--- makeit.sh
+++ makeit.sh
@@ -3,6 +3,7 @@
 find ~/.cache/pip/wheels/ -name '*nspace*' | xargs rm -f
 mkdir -p ~/nspace-dist
 rm -f  ~/nspace-dist/*
+rm -rf /tmp/testvenv
 
 if [ ! -f ~/.pip/pip.conf ] || ! grep -q nspace-dist ~/.pip/pip.conf ; then
   echo 'For this to work, create ~/.pip/pip.conf with the line:'
@@ -14,4 +15,12 @@ fi
 (cd nspace-two; python setup.py sdist -d ~/nspace-dist)
 (cd nspace-three; python setup.py sdist -d ~/nspace-dist)
 
-(cd nspace-two; tox )
+(
+ virtualenv -p $(which python3) /tmp/testvenv 
+ source /tmp/testvenv/bin/activate
+ pip install ~/nspace-dist/nspace-two-1.0.0.tar.gz
+ set -x
+ cd nspace-two
+ python -c 'from nspace.three.morestuff import *'  # Always works
+ python -c 'from nspace.two.stuff import *'        # Breaks on 3.5
+)
